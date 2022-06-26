eba052cfd0c3a1ea5b0cd8c2211f3bbf3d7664b0
RomanVinichenko <chicagezzz@gmail.com>
2022-06-26 21:07:18 +0300

    Updates

--- js/index.js
+++ js/index.js
@@ -1,1 +1,36 @@
-$(function () {});
+if ($(window).width() < 1200) {
+    $(document).on('click', 'a[href^="#"]', function (event) {
+        event.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top - 20
+        }, 500);
+    });
+} else {
+    $(document).on('click', 'a[href^="#"]', function (event) {
+        event.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top - 80
+        }, 500);
+    });
+}
+
+(function () {
+    emailjs.init('Peje_gKxXqc-rxd7b');
+})();
+
+window.onload = function () {
+    document.getElementById('contact-form').addEventListener('submit', function (event) {
+        event.preventDefault();
+        // generate a five digit number for the contact_number variable
+        this.contact_number.value = Math.random() * 100000 | 0;
+        // these IDs from the previous steps
+        emailjs.sendForm('service_8b26w4b', 'template_edsho5c', this)
+            .then(function () {
+                console.log('SUCCESS!');
+            }, function (error) {
+                console.log('FAILED...', error);
+            });
+    });
+}
